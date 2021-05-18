f0d5fc3a01b2481d7ffb746cc30f4f26d2876094
volodymyrmigdal <volodymyr.migdal@gmail.com>
2021-05-18 16:36:55 +0300

    extend step, add commands

--- step/hdf5find.s
+++ step/hdf5find.s
@@ -22,16 +22,47 @@ output = output.toString();
 let installation = fs.realpathSync( output.trim() );
 installation = path.resolve( installation, '../..' )
 
-if( process.argv[ 2 ] === 'version' )
+if( process.argv[ 2 ] === 'settings' )
 {
-  let settings = fs.readFileSync( path.join( installation, 'lib/libhdf5.settings' ) );
-  settings = settings.toString();
+  console.log( getSettings( installation ) );
+}
+else if( process.argv[ 2 ] === 'version' )
+{
+  let settings = getSettings( installation )
   let version = /HDF5 Version:(.*)/.exec( settings )
   console.log( version[ 0 ] );
+}
+else if( process.argv[ 2 ] === 'diagnostics' )
+{
+  console.log( `HDF5 Installation path: ${installation}` );
+  console.log( `\n${getSettings( installation )}` );
 }
 else
 {
   console.log( installation );
+}
+
+//
+
+function getSettings( installation )
+{
+  let paths =
+  [
+    path.join( installation, 'lib/libhdf5.settings' ),
+    path.join( installation, 'share/hdf5/libhdf5.settings' ),
+  ]
+
+  for( let i = 0; i < paths.length; i++ )
+  {
+    if( fs.existsSync( paths[ i ] ) )
+    return fs.readFileSync( paths[ i ] ).toString();
+  }
+
+  let h5cc = path.join( installation, 'bin/h5cc' );
+  let execPath = `${h5cc} -showconfig`;
+  console.log( `> ${execPath}` )
+  let output = exec( execPath, { stdio : 'pipe' } );
+  return output.toString();
 }
 
 
